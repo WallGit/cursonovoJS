6a6870eea320e449416f86444d0efe1e4645cc49
Silva <wallace.b.silva@capgemini.com>
2023-01-04 15:42:55 -0300

    upload

---
+++ 2_operacao_aritmetica/script.js
@@ -0,0 +1,6 @@
+console.log(typeof (2 + 3));
+console.log(typeof (5 - 12));
+console.log(typeof (4 * 4 ));
+console.log(typeof (4 / 4 ));
+console.log(typeof (5 % 4 ));
+console.log(typeof (6 % 4 ));
---
+++ 4_String/script.js
@@ -0,0 +1,4 @@
+console.log(typeof "Oi meu nome é wallace");
+console.log(typeof'Eu queria comprar um carro');
+console.log(typeof `teste`);
+console.log(typeof Infinity);
