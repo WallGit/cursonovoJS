490cd6a4d2e54acfa83ac17889343b44e56e709e
wallace <wallace.b.silva@capgemini.com>
2023-01-10 16:55:07 -0300

    arrays

---
+++ _Array_1/script.js
@@ -0,0 +1,13 @@
+let arr = [1, 4, 5, 7];
+let nomes = ['wallace', 'eumesmo', 'teste'];
+let bool = [true, false, true];
+
+
+let misturado = [1, 'wallace', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
+
+
---
+++ _Array_2/script.js
@@ -0,0 +1,13 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['wallace', 'eumesmo', 'teste'];
+let bool = [true, false, true];
+
+
+
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1 ]);
+
+
