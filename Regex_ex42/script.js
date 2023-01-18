cf53d7bfc6d9c3aa26babe6c6e0ce7506790558d
wallace <wallace.b.silva@capgemini.com>
2023-01-18 12:30:50 -0300

    novas aulas

--- Regex_ex42/script.js
+++ Regex_ex42/script.js
@@ -1,5 +1,15 @@
-const validarID = /\w+[ID]/;
+// const validarID = /\w+[ID]/;
 
-console.log(validarID.test("testeID"));
-console.log(validarID.test("www.horadecodar.com.br"));
-console.log(validarID.test("123456ID"));
+// console.log(validarID.test("testeID"));
+// console.log(validarID.test("www.horadecodar.com.br"));
+// console.log(validarID.test("123456ID"));
+
+
+const validarId = /\d+ID\b/;
+
+console.log(validarId.test("321561ID"));
+console.log(validarId.test("33232"));
+console.log(validarId.test("asd"));
+console.log(validarId.test("asdID"));
+console.log(validarId.test("ID"));
+console.log(validarId.test("123ID"));
